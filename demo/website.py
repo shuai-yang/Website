48142fd9a96643895f855e54882f963d457e1a10
Sharon Yang <shuai.sharon.yang@gmail.com>
2021-01-02 18:55:04 -0500

    Add files via upload

--- demo/website.py
+++ demo/website.py
@@ -11,4 +11,8 @@ def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False)  #http://localhost:5000/about/
+#Press ctrl+C to quit
+#PS E:\Python> python .\website.py
+#https://www.geeksforgeeks.org/introduction-and-installation-of-heroku-cli-on-windows-machine/
+#crtl+R to refresh host webpage to reflect new changes
